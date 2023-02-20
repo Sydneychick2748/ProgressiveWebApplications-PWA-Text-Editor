8a532e9b99465a48f9952a676e22ca17e8760e2e
ana <ana@anniecannons.com>
2023-02-20 11:24:05 -0800

    tried to get the assests to cache off line and no luck

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,42 +1,30 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    // Store the triggered events
-    window.deferredPrompt = event;
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Store the triggered events
+  window.deferredPrompt = event;
 
-    // Remove the hidden class from the button.
-    butInstall.classList.toggle('hidden', false);
+  // Remove the hidden class from the button.
+  butInstall.classList.toggle("hidden", false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
 
-    console.log('promptEvent', promptEvent);
-    if (!promptEvent) {
-        return;
-    }
+  if (!promptEvent) {
+    return;
+  }
 
-    // Show prompt
-    promptEvent.prompt();
+  // Show prompt
+  promptEvent.prompt();
 
-    // Reset the deferred prompt variable, since
-    // prompt() can only be called once.
-    window.deferredPrompt = null;
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
 
-    // Hide the install button.
-    butInstall.classList.toggle('hidden', true);
-
-    // Log user response to prompt.
-   
+  butInstall.classList.toggle("hidden", true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-    // console.log('App was installed successfully', event);
-        window.deferredPrompt= null;
-//         butInstall.classList.toggle('hidden', true);
- });
-
+window.addEventListener("appinstalled", (event) => {
+  // Clear prompt
+  window.deferredPrompt = null;
+});
